7020743147584687eb6572aefd1f25f2b8659d11
turtle4512 <67227559+turtle4512@users.noreply.github.com>
2025-06-10 00:13:39 +0900

    Fix nested backticks in event messages

--- test.js
+++ test.js
@@ -1,13 +1,26 @@
 import assert from 'node:assert/strict';
+import { esc, formatTx } from './helpers.js';
 
-// Same escaping function as in monitor.js
-const esc = (s) => s.replace(/([_*\[\]()~`>#+\-=|{}.!\\])/g, '\\$1');
-
+// tests for esc
 assert.equal(esc('_'), '\\_');
 assert.equal(esc('a_b'), 'a\\_b');
 assert.equal(esc('['), '\\[');
 assert.equal(esc(']'), '\\]');
 assert.equal(esc('('), '\\(');
 assert.equal(esc(')'), '\\)');
+
+// test formatTx helper
+const tx = {
+  from: '0x1111111111111111111111111111111111111111',
+  to: '0x2222222222222222222222222222222222222222',
+  value: 1n * 10n ** 18n,
+  hash: '0xabc'
+};
+
+const msg = formatTx(tx);
+assert(msg.includes(tx.from));
+assert(msg.includes(tx.to));
+assert(msg.includes('1'));
+assert(msg.includes(tx.hash));
 
 console.log('All tests passed!');
